22c2470e9c2774a989aeeaa3436e2b170b4dd4ca
yonatan.liling <yonatan.liling@cellebrite.com>
2020-12-13 09:13:04 +0200

    add api

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/client', '<rootDir>/libs/data'],
+  projects: [
+    '<rootDir>/apps/client',
+    '<rootDir>/libs/data',
+    '<rootDir>/apps/api',
+  ],
 };
